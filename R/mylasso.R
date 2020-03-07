929fada970d5f57ab21b1bb2976de746de2a2274
PriscillaBai-UTokyo <59157257+PriscillaBai-UTokyo@users.noreply.github.com>
2020-03-07 19:06:33 +0900

    mylasso

--- R/mylasso.R
+++ R/mylasso.R
@@ -1,14 +1,21 @@
-mylasso <- function(input,OStime,Status){
+mylasso <- function(data,OStime,OS){
   library("survival")
   library("glmnet")
-  cvfit = cv.glmnet(data, Surv(OStime,OS),
-                    nfold=10,
+  data = factor2num(data)
+  if(length(which(OStime == 0)) != 0)
+  {m=which(OStime == 0)
+  data = data[-m,]
+  OStime = OStime[-m]
+  OS = OS[-m]}
+  data = as.matrix(data)
+  cvfit = cv.glmnet(data, Surv(as.numeric(OStime),OS),
+                    nfolds=10,
                     family = "cox")
   pdf("lasso_plot1.pdf")
   plot(cvfit)
   dev.off()
   return(cvfit$lambda.min)
-  fit <- glmnet(t(myexpr), Surv(mysurv$months,mysurv$status),family = "cox")
+  fit <- glmnet(data, Surv(OStime,OS),family = "cox")
 
   pdf("lasso_plot2.pdf")
   plot(fit, label = TRUE)
@@ -16,7 +23,8 @@ mylasso <- function(input,OStime,Status){
 
   coef.min = coef(cvfit, s = "lambda.min")
   active.min = which(coef.min != 0)
-  geneids <- rownames(myexpr)[active.min]
+  index.min = coef.min[active.min]
+  geneids <- colnames(data)[active.min]
   combine <- cbind(geneids, index.min)
   write.csv(combine,"gene_index.csv")
   return(combine)
